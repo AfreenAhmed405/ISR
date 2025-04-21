04c6e42c0fc7d3819723246704a2d7e23a8bb334
rhea-19 <93151142+rhea-19@users.noreply.github.com>
2025-04-21 12:30:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, jsonify
 import os
 import json
 from tokenizer import process_pdf
